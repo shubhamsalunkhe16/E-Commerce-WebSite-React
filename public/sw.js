3f38b5fafb3016224cceb1fb734d697e5314e893
Shubham <salunkheshubham0@gmail.com>
2022-05-18 19:21:18 +0530

    updated manifest

--- public/sw.js
+++ public/sw.js
@@ -13,10 +13,10 @@ self.addEventListener('install', (event) => {
       '/static/js/main.chunk.js',
       'index.html',
       '/',
+      '/manifest.json',
       '/static/media/bannerImage2.7f11a80c.png',
       '/static/media/bannerImage3.75a7e2a0.jpg',
       'https://fakestoreapi.com/products',
-      '/Products',
     ])
   );
 });
@@ -32,28 +32,3 @@ this.addEventListener('fetch', (event) => {
     );
   }
 });
-
-var deferredPrompt;
-
-this.addEventListener('beforeinstallprompt', function (event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
-
-if (deferredPrompt) {
-  deferredPrompt.prompt();
-
-  deferredPrompt.userChoice.then(function (choiceResult) {
-    console.log(choiceResult.outcome);
-
-    if (choiceResult.outcome === 'dismissed') {
-      console.log('User cancelled installation');
-    } else {
-      console.log('User added to home screen');
-    }
-  });
-
-  deferredPrompt = null;
-}
