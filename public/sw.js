f83c6f498557ca9e62daacbcd96b5e41faeee201
Shubham <salunkheshubham0@gmail.com>
2022-05-19 14:51:27 +0530

    access location test 1

--- public/sw.js
+++ public/sw.js
@@ -49,29 +49,41 @@ self.addEventListener('activate', (evt) => {
 });
 
 // fetch events
-self.addEventListener('fetch', (evt) => {
-  if (evt.request.url.indexOf('firestore.googleapis.com') === -1) {
-    evt.respondWith(
-      caches
-        .match(evt.request)
-        .then((cacheRes) => {
-          return (
-            cacheRes ||
-            fetch(evt.request).then((fetchRes) => {
-              return caches.open(dynamicCacheName).then((cache) => {
-                cache.put(evt.request.url, fetchRes.clone());
-                // check cached items size
-                limitCacheSize(dynamicCacheName, 15);
-                return fetchRes;
-              });
-            })
-          );
-        })
-        .catch(() => {
-          if (evt.request.url.indexOf('.html') > -1) {
-            return caches.match('/pages/fallback.html');
-          }
-        })
+// self.addEventListener('fetch', (evt) => {
+//   if (evt.request.url.indexOf('firestore.googleapis.com') === -1) {
+//     evt.respondWith(
+//       caches
+//         .match(evt.request)
+//         .then((cacheRes) => {
+//           return (
+//             cacheRes ||
+//             fetch(evt.request).then((fetchRes) => {
+//               return caches.open(dynamicCacheName).then((cache) => {
+//                 cache.put(evt.request.url, fetchRes.clone());
+//                 // check cached items size
+//                 limitCacheSize(dynamicCacheName, 15);
+//                 return fetchRes;
+//               });
+//             })
+//           );
+//         })
+//         .catch(() => {
+//           if (evt.request.url.indexOf('.html') > -1) {
+//             return caches.match('/pages/fallback.html');
+//           }
+//         })
+//     );
+//   }
+// });
+
+self.addEventListener('fetch', (event) => {
+  if (!navigator.onLine) {
+    event.respondWith(
+      caches.match(event.request).then((result) => {
+        if (result) {
+          return result;
+        }
+      })
     );
   }
 });
--- src/Pages/TestPage/GetLocation.js
+++ src/Pages/TestPage/GetLocation.js
@@ -1,75 +1,113 @@
-// import React, { useState, useEffect } from 'react';
+// import React from 'react';
+
+// const [latitude, setLatitude] = useState(0);
+// const [longitude, setLongitude] = useState(0);
+
+// useEffect(() => {
+//   if ('geolocation' in navigator) {
+//     console.log('Available');
+//     if (navigator.geolocation) {
+//       navigator.geolocation.watchPosition(function (position) {
+//         setLatitude(position.coords.latitude);
+//         setLongitude(position.coords.longitude);
+//         console.log('Latitude is :', position.coords.latitude);
+//         console.log('Longitude is :', position.coords.longitude);
+//       });
+//     } else {
+//       alert('No permisson');
+//     }
+//   } else {
+//     console.log('Not Available');
+//   }
+// }, []);
 
 // const GetLocation = () => {
-//   const [latitude, setLatitude] = useState(0);
-//   const [longitude, setLongitude] = useState(0);
-
-//   useEffect(() => {
-//     if ('geolocation' in navigator) {
-//       console.log('Available');
-//       if (navigator.geolocation) {
-//         console.log(navigator.geolocation);
-//         navigator.geolocation.watchPosition((position) => {
-//           setLatitude(position.coords.latitude);
-//           setLongitude(position.coords.longitude);
-//           console.log('Latitude is :', position.coords.latitude);
-//           console.log('Longitude is :', position.coords.longitude);
-//         });
-//       } else {
-//         alert('No permisson');
-//       }
-//     } else {
-//       console.log('Not Available');
-//     }
-//   }, []);
 //   return (
 //     <div>
 //       <div>
 //         <h4>Using geolocation is: </h4>
-//         <h5>Latitude is : {latitude}</h5>
-//         <h5>Longitude is : {longitude}</h5>
+//         <h3>Latitude is : {latitude}</h3>
+//         <h3>Longitude is : {longitude}</h3>
 //       </div>
 //     </div>
 //   );
 // };
 
 // export default GetLocation;
+// import React from 'react';
+// import { geolocated } from 'react-geolocated';
 
-import React from 'react';
+// class GetLocation extends React.Component {
+//   render() {
+//     {
+//       console.log('this.props', this.props);
+//     }
+//     return !this.props.isGeolocationAvailable ? (
+//       <div>Your browser does not support Geolocation</div>
+//     ) : !this.props.isGeolocationEnabled ? (
+//       <div>Geolocation is not enabled</div>
+//     ) : this.props.coords ? (
+//       <table>
+//         <tbody>
+//           <tr>
+//             <td>latitude</td>
+//             <td>{this.props.coords.latitude}</td>
+//           </tr>
+//           <tr>
+//             <td>longitude</td>
+//             <td>{this.props.coords.longitude}</td>
+//           </tr>
+//           <tr>
+//             <td>altitude</td>
+//             <td>{this.props.coords.altitude}</td>
+//           </tr>
+//           <tr>
+//             <td>heading</td>
+//             <td>{this.props.coords.heading}</td>
+//           </tr>
+//           <tr>
+//             <td>speed</td>
+//             <td>{this.props.coords.speed}</td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     ) : (
+//       <div>Getting the location data&hellip; </div>
+//     );
+//   }
+// }
+
+// export default geolocated({
+//   positionOptions: {
+//     enableHighAccuracy: false,
+//   },
+//   userDecisionTimeout: 5000,
+// })(GetLocation);
+
+import React, { useRef } from 'react';
+import { Demo } from './Demo.jsx';
 
 const GetLocation = () => {
-  var watchID;
-  var geoLoc;
+  const innerRef = useRef();
 
-  function showLocation(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    alert('Latitude : ' + latitude + ' Longitude: ' + longitude);
-  }
+  const getLocation = () => {
+    innerRef.current && innerRef.current.getLocation();
+  };
 
-  function errorHandler(err) {
-    if (err.code == 1) {
-      alert('Error: Access is denied!');
-    } else if (err.code == 2) {
-      alert('Error: Position is unavailable!');
-    }
-  }
-
-  function getLocationUpdate() {
-    if (navigator.geolocation) {
-      // timeout at 60000 milliseconds (60 seconds)
-      var options = { timeout: 60000 };
-      geoLoc = navigator.geolocation;
-      watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
-    } else {
-      alert('Sorry, browser does not support geolocation!');
-    }
-  }
   return (
     <div>
-      <form>
-        <input type='button' onclick={getLocationUpdate} value='Watch Update' />
-      </form>
+      <div className='text-slate-900'>
+        <Demo onError={(error) => console.log(error)} ref={innerRef} />
+      </div>
+      <div className='flex items-center space-x-2'>
+        <button
+          className='bg-sky-600 hover:bg-sky-800 py-2 px-4 rounded-md'
+          onClick={getLocation}
+          type='button'
+        >
+          Get location
+        </button>
+      </div>
     </div>
   );
 };
