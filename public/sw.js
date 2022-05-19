ed7f513b7f4c5287dbeb6d151d079fd19c599493
Shubham <salunkheshubham0@gmail.com>
2022-05-19 12:04:03 +0530

    sw update

--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,3 @@
-// console.log('SW working');
-
-// const addResourcesToCache = async (resources) => {
-//   const cache = await caches.open('v1');
-//   await cache.addAll(resources);
-// };
-
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     addResourcesToCache([
-//       '/static/js/bundle.js',
-//       '/static/js/0.chunk.js',
-//       '/static/js/main.chunk.js',
-//       'index.html',
-//       '/',
-//       '/manifest.json',
-//       '/static/media/bannerImage2.7f11a80c.png',
-//       '/static/media/bannerImage3.75a7e2a0.jpg',
-//       'https://fakestoreapi.com/products',
-//     ])
-//   );
-// });
-
-// this.addEventListener('fetch', (event) => {
-//   if (!navigator.onLine) {
-//     event.respondWith(
-//       caches.match(event.request).then((result) => {
-//         if (result) {
-//           return result;
-//         }
-//       })
-//     );
-//   }
-// });
-
 const staticCacheName = 'site-static-v3';
 const dynamicCacheName = 'site-dynamic-v3';
 const assets = [
@@ -59,6 +24,7 @@ const limitCacheSize = (name, size) => {
 // install event
 self.addEventListener('install', (evt) => {
   //console.log('service worker installed');
+
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log('caching shell assets');
